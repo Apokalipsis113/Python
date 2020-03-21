72fe464159bbfd9a398952b988c4b0107aeac14d
apokalipsis113 <apokalipsis113@gmail.com>
2020-03-21 11:55:17 +0300

    Initial commit

---
+++ codewithmosh/vidly/movies/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Genge, Movie
+# Register your models here.
+
+
+class GengeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created',)
+    list_display = ('title', 'number_in_stock', 'daily_rate')
+
+
+admin.site.register(Genge, GengeAdmin)
+admin.site.register(Movie, MovieAdmin)
